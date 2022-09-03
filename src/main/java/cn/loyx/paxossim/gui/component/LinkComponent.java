a53d4d814eb87da983801b3b1360d4c1a98e8acc
loyx <isluoyixuan1@163.com>
2022-09-03 18:19:33 +0800

    :sparkles: feat(UI): optimize LinkComponent ui

--- src/main/java/cn/loyx/paxossim/gui/component/LinkComponent.java
+++ src/main/java/cn/loyx/paxossim/gui/component/LinkComponent.java
@@ -3,19 +3,28 @@ package cn.loyx.paxossim.gui.component;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
-import java.util.Random;
 
 public class LinkComponent extends JComponent {
     SiteComponent start;
     SiteComponent end;
+    private int lineWidth;
 
 
     public LinkComponent(SiteComponent start, SiteComponent end) {
+        this(start, end, 3);
+    }
+
+    public LinkComponent(SiteComponent start, SiteComponent end, int lineWidth){
         this.start = start;
         this.end = end;
+        this.lineWidth = lineWidth;
         this.start.registerLink(this);
         this.end.registerLink(this);
         updateLink();
+    }
+
+    public void setLineWidth(int lineWidth) {
+        this.lineWidth = lineWidth;
     }
 
     public void updateLink(){
@@ -32,22 +41,50 @@ public class LinkComponent extends JComponent {
         int minY = Math.min(y1, y2);
         int maxX = Math.max(x1, x2);
         int maxY = Math.max(y1, y2);
-        setLocation(minX, minY);
-        setSize(maxX - minX, maxY - minY);
+        setLocation((int) (minX + lineWidth/2.0), (int) (minY + lineWidth/2.0));
+        setSize(maxX - minX + lineWidth , maxY - minY + lineWidth);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2D = (Graphics2D) g.create();
+
+        // enable antialias
         g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2D.setStroke(new BasicStroke(3, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND,
+
+        // dash line
+        g2D.setStroke(new BasicStroke(lineWidth, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND,
                 1, new float[]{5}, 0));
+
+        // diag and back-diag
+        double[] pos;
+        double halfWidth = lineWidth / 2.0;
         if (start.getLocation().y < end.getLocation().y) {
-            g2D.draw(new Line2D.Double(0, 0, getWidth(), getHeight()));
+            pos = new double[]{0 + halfWidth, 0 + halfWidth, getWidth()- halfWidth, getHeight() - halfWidth};
         } else {
-            g2D.draw(new Line2D.Double(0, getHeight(), getWidth(), 0));
+            pos = new double[]{0 + halfWidth, getHeight() - halfWidth, getWidth() - halfWidth, 0 + halfWidth};
         }
+
+        // end margin
+        double yLen = Math.abs(end.getBounds().getCenterY() - start.getBounds().getCenterY());
+        double xLen = Math.abs(end.getBounds().getCenterX() - start.getBounds().getCenterX());
+        double diagLength = Math.sqrt(xLen * xLen + yLen * yLen);
+        double marginRatio;
+        if (xLen > yLen){
+            marginRatio = (start.getWidth()/2.0/ (xLen / diagLength)) / diagLength;
+        } else {
+            marginRatio = (start.getHeight()/2.0/ (yLen / diagLength)) / diagLength;
+        }
+        pos = new double[]{
+                pos[0] + marginRatio * (pos[2] - pos[0]),
+                pos[1] + marginRatio * (pos[3] - pos[1]),
+                pos[0] + (1 - marginRatio) * (pos[2] - pos[0]),
+                pos[1] + (1 - marginRatio) * (pos[3] - pos[1]),
+        };
+
+        // draw line
+        g2D.draw(new Line2D.Double(pos[0], pos[1], pos[2], pos[3]));
         g2D.dispose();
     }
 }
